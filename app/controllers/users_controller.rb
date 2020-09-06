230f853eb1e7d346ded46a66ed5985879f0b258a
Kazuya Haraguchi <k.haraguchi2000@gmail.com>
2020-09-06 12:20:12 +0000

    Add login and admin function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, only: [:edit, :update, :destroy]
+    before_action :require_same_user, only: [:edit, :update, :destroy, :destroy]
     
     def new
         @user = User.new
     end
     
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
     
@@ -16,6 +18,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome to the Alpha Blog, #{@user.username}. You have successfully signed up!"
             redirect_to articles_path
         else
@@ -23,13 +26,17 @@ class UsersController < ApplicationController
         end
     end
     
+    def destroy
+        @user.destroy
+        session[:user_id] = nil if @user == current_user
+        flash[:notice] = "Account and articles have been deleted."
+        redirect_to articles_path
+    end
+    
     def edit
-        @user = User.find(params[:id])
-        
     end
     
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "#{@user.username}, your profile has been updated."
             redirect_to @user
@@ -43,5 +50,16 @@ class UsersController < ApplicationController
     
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+    
+    def set_user
+        @user = User.find(params[:id])
+    end
+    
+    def require_same_user
+        if current_user != @user && !current_user.admin?
+            flash[:alert] = "You are not allowed to perform that action."
+            redirect_to @user
+        end
     end
 end
