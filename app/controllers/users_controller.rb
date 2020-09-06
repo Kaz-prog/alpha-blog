b04ff195d3eefbe16751dd5e67f5e9edc4d99348
Kazuya Haraguchi <k.haraguchi2000@gmail.com>
2020-09-06 07:53:59 +0000

    Add pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,11 @@ class UsersController < ApplicationController
     
     def show
         @user = User.find(params[:id])
-        @articles = @user.articles
+        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    end
+    
+    def index
+        @users = User.paginate(page: params[:page], per_page: 5)
     end
     
     def create
@@ -28,7 +32,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "#{@user.username}, your profile has been updated."
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end
